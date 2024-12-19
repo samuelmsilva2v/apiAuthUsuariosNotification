6d6111f13d4827c8b9ac682a93498f3bde833f01
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-12-19 14:25:38 -0300

    swagger config

---
+++ src/main/java/configurations/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	OpenAPI customOpenApi() {
+
+		OpenAPI openAPI = new OpenAPI().components(new Components())
+				.info(new Info().title("Auth Usuarios Notifications").description(" ").version("v1"));
+
+		return openAPI;
+	}
+}
